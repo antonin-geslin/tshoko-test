c436d61875a9420a5c68acfe5e63a22e5bb19dfa
Antonin Geslin Dugué <latouillette@MacBook-Air-de-Antonin.local>
2023-08-10 14:17:13 +0200

    push route put/search et axes d'amélioration

--- server.js
+++ server.js
@@ -41,8 +41,7 @@ app.get('/api/artists', (req, res) => {
     if (!artists) {
       res.status(404).json('artists not found');
     }
-    res.json(artists);
-    res.status(200);
+    res.status(200).json(artists);
 });
 
 //get artists by id
@@ -52,10 +51,21 @@ app.get('/api/artists/:id', (req, res) => {
     if (!artist) {
       res.status(404).json('artist not found');
     }
-    res.json(artist);
-    res.status(200);
+    res.status(200).json(artist);
 });
 
+
+//search artist 
+app.get('/api/artists/search/:name', (req, res) => {
+  const nameArtist = req.params.name.toUpperCase();
+  console.log(nameArtist)
+  const artist = artists.find(artist => artist.name.toUpperCase() === nameArtist);
+  console.log(artist);
+  if (!artist) {
+    res.status(404).json('artist not found');
+  }
+  res.json(artist);
+});
 
 //add artist
 app.post('/api/artists', (req, res) => {
@@ -68,9 +78,9 @@ app.post('/api/artists', (req, res) => {
   } 
   
   else {
-    // ajout au tableau
+    // add to tab
     artists.push(artist);
-    //mise à jour du json
+    //update json
     const updateArtists = JSON.stringify(artists); 
     fs.writeFile('./artists.json', updateArtists,'utf8', err => {
       if(err){
@@ -96,13 +106,12 @@ app.delete('/api/artists/:id', (req, res) => {
   
   else {
     var index = artists.indexOf(artist);
-    //mise à jour du tableau
+    //update tableau
     artists.splice(index, 1);
-    //mise à jour du json
+    //update json
     const updateArtists = JSON.stringify(artists);
     fs.writeFile('./artists.json', updateArtists,'utf8', err => {
       if(err){
-        console.error('Error writing JSON file:', err);
         res.status(500).json({ error: 'Internal server error' });
         return;
       }
@@ -112,3 +121,55 @@ app.delete('/api/artists/:id', (req, res) => {
 }
 );
 
+app.put('/api/artists/:id', (req, res) => {
+  //check artist exist
+  const idArtist = parseInt(req.params.id);
+  const artist = artists.find(artist => artist.id === idArtist);
+
+  if (!artist) {
+    res.status(404).json({ error: 'artist not found' });
+    return;
+  }
+
+  //catch entry data
+  else {
+    const artistUpdate = req.body;
+    if (!artistUpdate || !artistUpdate.image || !artistUpdate.name || !artistUpdate.albumsCount || !artistUpdate.id) {
+      res.status(400).json({ error: 'bad request' });
+      return;
+    }
+    else {
+      var index = artists.indexOf(artist);
+      //update tab data
+      artists.splice(index, 1, artistUpdate);
+      //update json
+      const updateArtists = JSON.stringify(artists);
+      fs.writeFile('./artists.json', updateArtists,'utf8', err => {
+        if(err){
+          res.status(500).json({ error: 'Internal server error' });
+          return;
+        }
+        res.status(200).json(artistUpdate);
+      });
+    }
+  }
+}
+);
+
+
+
+
+
+
+/*
+- Que faire si on a 1 000 000 d’artistes ? Utiliser une vraie db (mongoDB)
+- Comment modifier un artiste existant ? Avec une route PUT
+- Comment améliorerez vous votre api ? (sécurité, architecture, qualité de code, maintenabilité etc…)
+
+- a chaque modification le fichier json est rééecrit entièrement à partir du tableau ce qui n'est pas très optimal pour de grandes données
+-authentification de l'utilisateur qui utilise l'api
+-chiffrement des données entre client et serveur
+-modèle de données dans un autre fichier pour vérif des données entrantes (mongoDB)
+-protection injection SQL
+
+*/
